17abc87668a47301ea2c99f7a59725e34b5a6902
Cyronic <74363987+CyronicArts@users.noreply.github.com>
2022-01-16 21:47:19 +0100

    Implement Cartridge Interaction
    + fix grab orientation

--- ER-P3_ProjectING/Assets/Scripts/Cartridge.cs
+++ ER-P3_ProjectING/Assets/Scripts/Cartridge.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Cartridge : MonoBehaviour
 {
+
+    public bool isGrabbed;
+    public bool isInserted;
+    public GameObject cartridgeTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGrabbed = false;
+        isInserted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isInserted == true)
+        {
+            this.transform.rotation = cartridgeTransform.transform.rotation;
+            this.transform.position = cartridgeTransform.transform.position;
+        }
+
+        if (isInserted && isGrabbed)
+        {
+            isInserted = false;
+        }
+
+        Debug.Log(isGrabbed);
     }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "CartridgeTransform" && !isGrabbed)
+        {
+            this.transform.rotation = cartridgeTransform.transform.rotation;
+            this.transform.position = cartridgeTransform.transform.position;
+            isInserted = true;
+        }
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "CartridgeTransform" && !isGrabbed)
+        {
+            this.transform.rotation = cartridgeTransform.transform.rotation;
+            this.transform.position = cartridgeTransform.transform.position;
+            isInserted = true;
+        }
+    }
+
+    public void SetIsGrabbed(bool _isGrabbed)
+    {
+        isGrabbed = _isGrabbed;
+    }
+
 }
