447552992edd3c194aed1087c24c271ff964fff7
YaniKou <77344945+YaniKou@users.noreply.github.com>
2022-01-21 13:28:25 +0100

    Imported Play Area
    Bugs: Lantern and cartridges go crazy and fly out

--- ER-P3_ProjectING/Assets/Scripts/VoidCheck.cs
+++ ER-P3_ProjectING/Assets/Scripts/VoidCheck.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class VoidCheck : MonoBehaviour
 {
+    public GameObject lantern;
+
+    private Transform initialLanternTransform;
+
+    private void Start ()
+    {
+        initialLanternTransform = lantern.transform.position;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "VoidCheck")
+        //other.transform.position += new Vector3(0, 10, 0);
+
+        if (other.tag == Lantern)
         {
-            this.transform.position += new Vector3(0, 10, 0);
+            lantern.transform.position = initialLanternTransform;
         }
     }
 }
