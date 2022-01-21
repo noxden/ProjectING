d7aab6f2767e0ab454c71f47d3856721f84eba71
YaniKou <77344945+YaniKou@users.noreply.github.com>
2022-01-21 20:07:51 +0100

    Build up play area with 6 walls

--- ER-P3_ProjectING/Assets/Scripts/VoidCheck.cs
+++ ER-P3_ProjectING/Assets/Scripts/VoidCheck.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class VoidCheck : MonoBehaviour
 {
-    public GameObject lantern;
-
-    private Transform initialLanternTransform;
-
-    private void Start ()
-    {
-        initialLanternTransform = lantern.transform.position;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        //other.transform.position += new Vector3(0, 10, 0);
-
-        if (other.tag == Lantern)
+        if (other.tag == "VoidCheck")
         {
-            lantern.transform.position = initialLanternTransform;
+            this.transform.position += new Vector3(0, 10, 0);
         }
     }
 }
